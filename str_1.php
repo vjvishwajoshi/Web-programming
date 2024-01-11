a5e2da94153619571cace55e6722757c44a51845
vjvishwajoshi <155036425+vjvishwajoshi@users.noreply.github.com>
2024-01-11 08:57:12 +0530

    Add files via upload

---
+++ str_1.php
@@ -0,0 +1,47 @@
+<?php
+#String Functions:
+
+#strlen:
+
+echo strlen("Hello, World!")."<br>";
+echo strlen("Good Morning...!")."<br>";
+
+#strpos":
+echo strpos("Hello, World!","World")."<br>";
+echo strpos("Welcome to calss B1","to")."<br>";
+
+#str_replace:
+
+echo str_replace("World","Universe", "Hello, World!")."<br>";
+echo str_replace("python","Oracle","I like python...")."<br>";
+
+#substr:
+
+echo substr("Hello, World!", 0, 5)."<br>";
+echo substr("Hello, Good Morning...!", 7, 10)."<br>";
+
+#strtolower:
+
+echo strtolower("Hello, World!")."<br>";
+echo strtolower("I Am Here...")."<br>";
+
+#strtoupper:
+
+echo strtoupper("Hello, World!")."<br>";
+echo strtoupper("Vishwa")."<br>";
+
+#ucfirst:
+
+echo ucfirst("hello, world!")."<br>";
+echo ucfirst("come here...")."<br>";
+
+#strrev:
+
+echo strrev("Hello, World!")."<br>";
+echo strrev("Good Morning...")."<br>";
+
+#trim:
+
+echo trim(" Hello, World! ")."<br>";
+echo trim(" ***Good Morning");
+?>
